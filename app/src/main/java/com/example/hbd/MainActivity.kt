2a329d1f560704385188c612c573d3b042fbcfb9
muhammadjordansky <muhammad.jordan@students.utdi.ac.id>
2022-09-23 14:18:47 +0700

    commit penjelasan

--- app/src/main/java/com/example/hbd/MainActivity.kt
+++ app/src/main/java/com/example/hbd/MainActivity.kt
@@ -1,6 +1,8 @@
+//Berfungsi sebagai pemanggil paket com.example.hbd
 package com.example.hbd
-
+//Berfungsi untuk import kelas androidx.appcompat.appAppCompatActivity
 import androidx.appcompat.app.AppCompatActivity
+//Berfungsi untuk impor kelas android.os.Bundle
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
